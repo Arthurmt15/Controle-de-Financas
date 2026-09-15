ed16814b801370a8d74bbc0340be3ad17f9299f3
arthur.oliveira <@arthur.oliveira>
2026-09-15 09:09:45 -0300

    fix(build): desativa jsx-a11y/heading-has-content em CardTitle para build CI
    - Vercel trata warnings como errors (CI=true) e falhou em card.tsx:15
    - CardTitle e wrapper generico com props.children, falso positivo do eslint
    - adiciona eslint-disable no topo do arquivo com comentario explicativo

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/heading-has-content -- CardTitle é wrapper genérico, conteúdo vem via props.children */
 import * as React from "react"
 import { cn } from "../../lib/utils"
 
